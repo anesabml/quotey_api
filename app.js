35dc20279460d48d1ab7d6f44ccbeeb5dd861218
anesabml <anesabismail@gmail.com>
2020-02-14 18:46:14 +0100

    Add Procfile

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 
 const quotes = require("./quotes.json");
 
-// Add this to simplify getting data from a post request
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ type: "application/json" }));
 // Add this to access html files
 app.use(express.static(__dirname + "/public"));
 
@@ -15,7 +11,6 @@ app.use(express.static(__dirname + "/public"));
 app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
